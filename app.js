d9fa5eda90ec8a55fc890244dfd0d0bc328a8b63
wesbin <tktma12>
2020-12-16 16:03:13 +0900

    ejs setting

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/**
+ * Router Description
+ */
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+/**
+ * View Engine Description
+ */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/**
+ * Other Setting
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Router Description
+ */
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
